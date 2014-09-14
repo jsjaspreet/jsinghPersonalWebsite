32c491fc3a49ecf5e8dbb9f1f952856f1bddeb6d
Jaspreet Singh <jsjaspreet@berkeley.edu>
2014-09-13 20:32:07 -0700

    Add production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'google.com',
+      user_name:            ENV["GMAIL_USERNAME"],
+      password:             ENV["GMAIL_PWD"],
+      enable_starttls_auto: true,
+      authentication:       'plain',
+  }
 end
